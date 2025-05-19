59e7a9088d4553821f8ca7f7cbd19538ac4accb6
Tu Quang Chuong <22130029@st.hcmuaf.edu.vn>
2025-05-20 00:16:02 +0700

    update da call api voi backend thanh cong

--- lib/services/lesson_service.dart
+++ lib/services/lesson_service.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models/Lesson.dart';
 
-/* Tu Quang Chuong thuc hien
+/* Tu Quang Chuong thuc hien 
 4.1.2: Gửi GET lấy danh sách bài học.
 4.1.8: Gửi POST xác nhận chọn bài học.*/
 
@@ -13,11 +13,11 @@ class LessonService {
 // 4.1.2: Gửi GET lấy danh sách bài học
   static Future<List<Lesson>> fetchLessons(String token) async {
     final response = await http.get(
-      Uri.parse('http://localhost:8080/api/lessons'),
+           Uri.parse('http://localhost:8080/api/lessons'),
       headers: {'Authorization': 'Bearer $token'},
     );
     if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
+     final List<dynamic> data = json.decode(utf8.decode(response.bodyBytes));
       return data.map((json) => Lesson.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load lessons');
